014965c6ad3110e1dacde31980965d05371ab980
Jankees van Woezik <jankees@base42.nl>
2012-08-13 18:46:28 +0200

    Rebased from develop

---
+++ db/migrate/20120813162212_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :name
+      t.string :title
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
