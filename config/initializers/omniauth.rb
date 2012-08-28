4970bc088ed6c9b2063f76f517cd00fda7d38e70
Jankees van Woezik <jankees@base42.nl>
2012-08-28 12:25:02 +0200

    Working on facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, APP_CONFIG[:facebook_key], APP_CONFIG[:facebook_secret]
+  provider :facebook, APP_CONFIG["facebook_key"], APP_CONFIG["facebook_secret"]
 end
