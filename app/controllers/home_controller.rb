555122e020f2f681ae4767edde0ff44c0d0ab0f6
Jankees van Woezik <jankees@base42.nl>
2012-08-30 15:46:16 +0200

    Added tweets to the homepage (tweets get cached for an hour). We use the favorited tweets on the @twelve_20 account

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,27 @@
 class HomeController < ApplicationController
+
+  require 'net/http'
+  require 'uri'
+
   def index
     @blogs = Blog.all :limit => 2
     @goody = Goody.first
+
+    unless Rails.cache.read('tweets')
+      # load json from twitter
+      uri = URI('http://api.twitter.com/1/favorites.json?screen_name=twelve_20')
+      response = Net::HTTP.get(uri)
+
+      #parse json
+      tweets = JSON.parse(response)
+
+      #save to cache
+      Rails.cache.write('tweets', tweets, :time_to_idle => 60.seconds, :expires_in => 1.hour)
+
+      logger.debug "Fetch new tweets."
+    end
+
+    @tweets = Rails.cache.read('tweets')
+
   end
 end
