e73de3b40902a0073f7132eb902db3a5eb79a103
Jankees van Woezik <jankees@base42.nl>
2012-09-03 09:23:07 +0200

    Cleaned the Routes file, also added redirection for old urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,38 @@
 Website::Application.routes.draw do
 
-  resources :artworks
-
-  resources :goodies
-
+  # apps
   match 'apps/:app' => 'apps#show', :constraints => {:name => /(gettogether|willitfit|uberlayer)/i}
+
+  # - old urls
+  match 'uberlayer', to: redirect('/apps/uberlayer')
+  match 'willitfit', to: redirect('/apps/willitfit')
+  match 'gettogether', to: redirect('/apps/gettogether')
+
+  # - apps with dashes
+  match 'will-it-fit', to: redirect('/apps/willitfit')
+  match 'get-together', to: redirect('/apps/gettogether')
+
+  # - apps abbreviations
+  match 'ul', to: redirect('/apps/uberlayer')
+  match 'wf', to: redirect('/apps/willitfit')
+  match 'gt', to: redirect('/apps/gettogether')
+
+  # static pages
   match 'contact' => 'contact#index'
   match 'about' => 'about#index'
   match 'home' => 'home#index', :path => '/'
 
-  root to: 'home#index'
-
+  # restful resources
+  resources :artworks
+  resources :goodies
   resources :blogs, :path => 'blog'
 
   #OmniAuth methods
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
+
+  # root url
+  root to: 'home#index'
 
 end
