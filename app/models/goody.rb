060237dcd68a4364f397828aa13ff711da4e36cc
Jankees van Woezik <jankees@base42.nl>
2012-09-03 11:46:03 +0200

    Added slug to goodies

--- app/models/goody.rb
+++ app/models/goody.rb
@@ -1,4 +1,8 @@
 class Goody < ActiveRecord::Base
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
   attr_accessible :author, :body, :download_url, :teaser, :artwork, :title, :draft
   mount_uploader :artwork, ImageUploader
 end
