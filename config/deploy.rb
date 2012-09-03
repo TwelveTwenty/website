6412b393dd0eacf4b70744805b7569f4b7220b57
Jankees van Woezik <jankees@base42.nl>
2012-09-03 19:50:42 +0200

    Added humans.txt and new deployment script (Capistrano)

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,21 +26,37 @@ namespace :passenger do
   task :restart do
     run "touch #{current_path}/tmp/restart.txt"
   end
+  desc "Access log files"
   task :logs do
     run "cd #{current_path}/log && tail -f -n 50 *.log"
   end
 end
 
-namespace :deploy do
-  task :symlink_shared do
-    run "rm #{release_path}/config/database.yml"
-    run "ln -fs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+namespace :application do
 
+  desc "Remove config from Git repository"
+  task :remove_config do
+    run "rm #{release_path}/config/database.yml"
     run "rm #{release_path}/config/config.yml"
+  end
+
+  desc "Copy real config files"
+  task :create_symlinks do
+    run "ln -fs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -fs #{shared_path}/config/config.yml #{release_path}/config/config.yml"
     run "ln -fs #{shared_path}/uploads #{release_path}/public/uploads"
   end
+
+  desc "Add deploy details to app folder"
+  task :write_deploy_details do
+    run "echo \"Last deployment: `date -R`\" > #{release_path}/public/deployment.txt"
+    run "echo \"Git commit hash: `cat #{current_path}/REVISION` \" >> #{release_path}/public/deployment.txt"
+    run "echo \"Github: https://github.com/TwelveTwenty/website/commit/`cat #{current_path}/REVISION` \" >> #{release_path}/public/deployment.txt"
+  end
+
 end
 
-after :deploy, "deploy:symlink_shared"
+after :deploy, "application:remove_config"
+after :deploy, "application:create_symlinks"
+after :deploy, "application:write_deploy_details"
 after :deploy, "passenger:restart"
