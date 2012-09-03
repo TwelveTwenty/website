8f626881bbb7a29766a7aa7156f0f24ec3c87538
Jankees van Woezik <jankees@base42.nl>
2012-09-03 16:22:02 +0200

    Dynamic page title for all pages and sharing to Twitter and Facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :hard_wrap => true)
     markdown.render(text).html_safe
   end
+
+  def title(page_title)
+    page_title = page_title
+    content_for(:title) { "#{page_title} - Twelve Twenty" }
+    @page_title = page_title
+  end
 end
